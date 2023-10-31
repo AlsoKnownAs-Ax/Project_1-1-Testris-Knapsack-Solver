97ae01214dedb187cd91d87b65161d2b30c5bc24
AlsoKnownAs-Ax <andreiamzu@yahoo.com>
2023-10-31 16:12:08 +0100

    Organized Packages

--- src/PentominosUtils/Search.java
+++ src/PentominosUtils/Search.java
@@ -1,18 +1,8 @@
 package PentominosUtils;
 
 
- import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Scanner;
-
-
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
 
 /**
  * This class includes the methods to support the search of a solution.
@@ -415,45 +405,5 @@ public class Search
 	 * Main function. Needs to be executed to start the basic search algorithm
 	 */
 
-	 public static Container container;
-	 public static Font titleFont = new Font("Rockwell", Font.PLAIN, 69);
-	 public static Font normalFont = new Font("Times New Roman", Font.PLAIN,26);
-	 public static final Font ChoiceFont = new Font("Times New Roman", Font.PLAIN,19);
-	 public static Font MainTextFont = new Font("Times New Roman", Font.PLAIN,30);
-	 public static Font PIXELED_NORMAL;
-	 public static int ItemsAmount = 0;
- 
-	 public static boolean[] FreeSlots = {true,true,true,true,true,true,true,true,true,true};
 
-    public static void main(String[] args)
-    {
-		long startTime = System.currentTimeMillis();
-        search();
-		long endTime = System.currentTimeMillis();
-
-		long millis = endTime - startTime;
-		System.out.println("EXECUTION TIME: " + millis + "ms");
-
-		try {
-            //create the font to use. Specify the size!
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/TitleFont.ttf")).deriveFont(42f);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-
-            Font customFont2 = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/Pixels.ttf")).deriveFont(52f);
-            Font Pixeled = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/Pixels.ttf")).deriveFont(34f);
-
-            //register the font test
-            ge.registerFont(customFont);
-            ge.registerFont(customFont2);
-            ge.registerFont(Pixeled);
-
-            titleFont = customFont;
-            MainTextFont = customFont2;
-            PIXELED_NORMAL = Pixeled;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch(FontFormatException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- src/Screens/TitleScreen.java
+++ src/Screens/TitleScreen.java
@@ -1,6 +1,10 @@
 package Screens;
 
+import PentominosUtils.Search;
+import MainApp.TetrisGame;
+
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,9 +24,25 @@ public class TitleScreen
     private JButton startButton;
     private TitleScreenHandler tsHandler = new TitleScreenHandler();
 
+    private Color BLACK = Color.black;
+    private Color RED   = Color.red;
+    private Color BLUE  = Color.blue;
+    private Color WHITE = Color.white;
+
+    private Container container = TetrisGame.container;
+
+
     public TitleScreen()
     {
+        window = new JFrame();
+        window.setSize(600,1000);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(BLACK);
+        window.setLayout(null);
+        container = window.getContentPane();
 
+        TitleNamePanel = new JPanel();
+        TitleNamePanel.setBounds(70,100,700,150);
     }
 
     private void setFonts(){
